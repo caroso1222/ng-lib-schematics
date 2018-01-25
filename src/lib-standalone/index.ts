024596939e6106e11c081e391b1454ade9d8b5fd
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2018-01-25 00:24:02 -0500

    feat: add package.json manipulation

--- src/lib-standalone/index.ts
+++ src/lib-standalone/index.ts
@@ -1,13 +1,239 @@
 import {
   Rule,
   SchematicContext,
-  Tree
+  Tree,
+  apply,
+  url,
+  filter,
+  noop,
+  template,
+  move,
+  chain,
+  branchAndMerge,
+  mergeWith,
+  UpdateRecorder,
 } from '@angular-devkit/schematics';
 import { Schema as LibraryOptions } from './schema';
+import { 
+  normalize,
+  parseJsonAst,
+  JsonAstObject,
+  JsonValue,
+} from "@angular-devkit/core";
+import * as stringUtils from "@angular-devkit/core/src/utils/strings";
 
+const DEV_DEPENDENCIES = [
+  { name: 'del', version: '^3.0.0' },
+  { name: 'fs', version: '0.0.1-security' },
+  { name: 'gulp', version: '^3.9.1' },
+  { name: 'gulp-clean-css', version: '^3.9.2' },
+  { name: 'gulp-htmlmin', version: '^4.0.0' },
+  { name: 'gulp-if', version: '^2.0.2' },
+  { name: 'gulp-replace', version: '^0.6.1' },
+  { name: 'gulp-sass', version: '^3.1.0' },
+];
 
 export default function LibStandalone(options: LibraryOptions): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+  options.path = options.path ? normalize(options.path) : options.path;
+
+  const templateSource = apply(url("./files/lib"), [
+    template({
+      ...stringUtils,
+      ...options
+    }),
+    move(options.sourceDir as string)
+  ]);
+
+  const gulpfile = apply(url("./files/gulpfile"), [ move('') ]);
+
+  return chain([
+    mergeWith(templateSource),
+    mergeWith(gulpfile),
+    addBuildScriptToManifest(),
+    addDevDependencies()
+  ]);
+}
+
+/**
+ * Adds the build script to the package.json manifest inside npm scripts:
+ * "build:lib": "gulp --gulpfile gulpfile.lib.js"
+ */
+function addBuildScriptToManifest(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const packageJsonPath = './package.json';
+
+    let packageJsonContent = tree.read(packageJsonPath);
+    if (!verifyPackageJson(tree, context, packageJsonContent)) {
+      return;
+    }
+
+    const parsed = JSON.parse(packageJsonContent.toString('utf-8'));
+    if (parsed.scripts['build:lib']) {
+      context.logger.warn('Existing \'build:lib\' npm script found.' + 
+      '\nYou will need to run the build command as ' +
+      '\'gulp --gulpfile gulpfile.lib.js\'`');
+      return;
+    }
+
+    const packageJsonAst = parseJsonAst(packageJsonContent.toString('utf-8'));
+
+    if (packageJsonAst.kind !== 'object') {
+      throw new Error('Invalid "package.json" file.');
+    }
+
+    const scriptsToken = getJsonToken(packageJsonAst, 'scripts');
+    if (scriptsToken) {
+      const recorder = tree.beginUpdate(packageJsonPath);
+      appendPropertyInAstObject(
+        recorder,
+        scriptsToken,
+        'build:lib',
+        'gulp --gulpfile gulpfile.lib.js'
+      );
+      tree.commitUpdate(recorder);
+      return tree;
+    }
+  }
+}
+
+/**
+ * Adds a propertyName: value inside of a json file
+ */
+function appendPropertyInAstObject(
+  recorder: UpdateRecorder,
+  node: JsonAstObject,
+  propertyName: string,
+  value: JsonValue,
+  indent = 4,
+) {
+  const indentStr = '\n' + new Array(indent + 1).join(' ');
+
+  if (node.properties.length > 0) {
+    // Insert comma.
+    const last = node.properties[node.properties.length - 1];
+    recorder.insertRight(last.start.offset + last.text.replace(/\s+$/, '').length, ',');
+  }
+
+  recorder.insertLeft(
+    node.end.offset - 1,
+    '  '
+    + `"${propertyName}": ${JSON.stringify(value, null, 2).replace(/\n/g, indentStr)}`
+    + indentStr.slice(0, -2),
+  );
+}
+
+/**
+ * Adds a several propertyName: value inside of a json file
+ */
+function appendDependenciesInAstObject(
+  recorder: UpdateRecorder,
+  node: JsonAstObject,
+  dependencies: DevDependency[],
+  indent = 4,
+) {
+  const indentStr = '\n' + new Array(indent + 1).join(' ');
+
+  if (node.properties.length > 0) {
+    // Insert comma.
+    const last = node.properties[node.properties.length - 1];
+    recorder.insertRight(last.start.offset + last.text.replace(/\s+$/, '').length, ',');
+  }
+
+  let textToInsert = '';
+  for (let i = 0; i < dependencies.length; i++) {
+    const dep = dependencies[i];
+    textToInsert += 
+    '  '
+    + `"${dep.name}": ${JSON.stringify(dep.version, null, 2).replace(/\n/g, indentStr)}`
+    + indentStr.slice(0, -2);
+    if (i === dependencies.length) {
+      textToInsert += ',';
+    }
+  }
+
+  recorder.insertLeft(
+    node.end.offset - 1,
+    textToInsert,
+  );
+}
+
+/**
+ * Adds the dependencies to the package.json needed to build the library
+ */
+function addDevDependencies(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const packageJsonPath = './package.json';
+
+    let packageJsonContent = tree.read(packageJsonPath);
+    if (!verifyPackageJson(tree, context, packageJsonContent)) {
+      return;
+    }
+
+    const parsedDevDependencies = JSON.parse(packageJsonContent.toString('utf-8')).devDependencies;
+    if (!parsedDevDependencies) {
+      return;
+    }
+    const currentDevDependencies = Object.keys(parsedDevDependencies);
+
+    const packageJsonAst = parseJsonAst(packageJsonContent.toString('utf-8'));
+
+    if (packageJsonAst.kind !== 'object') {
+      throw new Error('Invalid "package.json" file.');
+    }
+
+    const dependencyToken = getJsonToken(packageJsonAst, 'devDependencies');
+    if (dependencyToken) {
+      const recorder = tree.beginUpdate(packageJsonPath);
+      // analyze the devDependencies and add only the missing ones
+      // for (const dep of DEV_DEPENDENCIES) {
+      //   if (currentDevDependencies.indexOf(dep.name) === -1) {
+      //     appendPropertyInAstObject(
+      //       recorder,
+      //       dependencyToken,
+      //       dep.name,
+      //       dep.version
+      //     );
+      //   }
+      // }
+      appendDependenciesInAstObject(recorder, dependencyToken, DEV_DEPENDENCIES);
+      tree.commitUpdate(recorder);
+      return tree;
+    }
+  }
+}
+
+/**
+ * Verifies the package.json exists
+ */
+function verifyPackageJson(
+  tree: Tree,
+  context: SchematicContext,
+  fileContent: Buffer | null
+): fileContent is Buffer {
+  if (!fileContent) {
+    context.logger.warn(`package.json not found. You will need to run 
+    the build command as 'gulp --gulpfile gulpfile.lib.js'`);
+    return false;
+  }
+  return true;
+}
+
+export interface DevDependency {
+  name: string;
+  version: string;
+}
+
+/**
+ * Returns the json token of a property inside a json object
+ */
+function getJsonToken(packageJsonAst: JsonAstObject, key: string): JsonAstObject | null {
+  for (const property of packageJsonAst.properties) {
+    if (property.key.value == key) {
+      if (property.value.kind !== 'object') {
+        throw new Error(`Invalid "package.json" file. "${key}" needs to be an object`);
+      }
+      return property.value;
+    }
+  }
+  return null;
 }
--- src/lib-standalone/schema.d.ts
+++ src/lib-standalone/schema.d.ts
@@ -3,4 +3,14 @@ export interface Schema {
    * The name of the library.
    */
   name: string;
+
+  /**
+   * The path of the app source directory.
+   */
+  sourceDir?: string;
+
+  /**
+   * The path of the library source directory.
+   */
+  path?: string;
 }
